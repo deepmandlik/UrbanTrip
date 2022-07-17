4e41a01be58f63944c124df462a30c698ded7491
Deep Mandlik <mandlikdeep20@gmail.com>
2022-07-17 15:08:56 +0530

    fix: env variable issue

--- constants/apikey.tsx
+++ constants/apikey.tsx
@@ -1,4 +1,4 @@
-export const MAPBOX_API_KEY = process.env.MAPBOX_API_KEY;
-export const MAPBOX_STYLE_URL = process.env.MAPBOX_STYLE_URL;
-export const X_RAPIDAPI_KEY = process.env.X_RAPIDAPI_KEY;
-export const X_RAPIDAPI_HOST = process.env.X_RAPIDAPI_HOST;
+export const MAPBOX_API_KEY = process.env.NEXT_PUBLIC_MAPBOX_API_KEY;
+export const MAPBOX_STYLE_URL = process.env.NEXT_PUBLIC_MAPBOX_STYLE_URL;
+export const X_RAPIDAPI_KEY = process.env.NEXT_PUBLIC_X_RAPIDAPI_KEY;
+export const X_RAPIDAPI_HOST = process.env.NEXT_PUBLIC_X_RAPIDAPI_HOST;
